60fe1120aa4276a59145b6bda7267e716c9c523b
yzq1997 <18356309212@163.com>
2020-03-01 22:02:31 +0800

    第三个版本，写到倒计时抢购

--- src/js/index.js
+++ src/js/index.js
@@ -25,3 +25,35 @@ mySwiper.el.onmouseover = function(){
 mySwiper.el.onmouseout = function(){
     mySwiper.autoplay.start();
 }     
+
+
+//倒计时
+function getTwo(n){
+  return n<10 ? '0'+n : n;
+}
+// 获取到页面这元素
+var nums = document.getElementsByClassName('num');
+// 声明未来时间
+var future_time = new Date('2020-3-3 21:55:50');
+function print_time(){
+    // 声明现在时间
+    var now_time = new Date();
+    // 声明剩余时间
+    var remaining_time = Math.ceil( (future_time - now_time)/1000 );// 3155.357  => 3156  10089
+    // 检测剩余时间
+    if( remaining_time>=0 ){
+         // 对秒钟数进行时间的换算
+        var hour = Math.floor(remaining_time/3600);
+        var min = Math.floor(remaining_time%3600/60);
+        var sec = remaining_time%60;
+        // 组装出时分秒的字符串
+        var str = ''+getTwo(hour)+getTwo(min)+getTwo(sec);
+        // 遍历字符串，将每一个字符串放入对应的div中
+        for( var i=0;i<str.length;i++ ){
+          nums[i].innerText = str.charAt(i);
+        }
+        // console.log("还剩"+hour+"时"+min+"分"+sec+"秒");
+    }
+   
+}
+var timer = setInterval(print_time,1000);
